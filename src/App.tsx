d5894b5ebee145d2a58cef3604737160d60c6670
marcos <marcos.cosmoski@gmail.com>
2022-09-03 20:52:48 +0100

    feat: react modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { createServer } from "miragejs";
+import Modal from 'react-modal'
+import { useState } from "react";
 
 
 createServer({
@@ -49,10 +51,27 @@ createServer({
 
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal () { 
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal () { 
+    setIsNewTransactionModalOpen(false)
+  }
+  
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+
       <Dashboard /> 
+
+      <Modal isOpen={isNewTransactionModalOpen} 
+               onRequestClose={handleCloseNewTransactionModal}>
+      </Modal>
+
       <GlobalStyle />
 
     </>    
